5c8a0024096a8186c4a9c36743b9a7461471619a
Josh Bradley <josh-bradley@outlook.com>
2020-01-20 08:25:23 +1100

    Adding basic validation
    * Checking url looks like a apple music store url
    * Checking when the head element could not be found in the html of the
    apple page

--- TeamsSpotifyBot.fs
+++ TeamsSpotifyBot.fs
@@ -15,22 +15,32 @@ type TeamsSpotifyBot =
    override this.OnMessageActivityAsync(turnContext: ITurnContext<IMessageActivity> , cancellationToken: CancellationToken) =
         turnContext.Activity.RemoveRecipientMention() |> ignore
 
-        let url = this.GetAppleIdentifiers (turnContext.Activity.Text.Trim()) |> this.GetSpotifyUrl
-        let message = MessageFactory.Text(sprintf "FTFY\n[%s](%s)" url url)
+        let failedMessage = "Could not find it"
+        let appleUrl = turnContext.Activity.Text.Trim()
+        let messageText = appleUrl.StartsWith("https://music.apple.com")
+                            |> function
+                                | false -> failedMessage
+                                | true -> this.GetAppleIdentifiers (turnContext.Activity.Text.Trim())
+                                            |> function
+                                                | Some x ->
+                                                    let url = this.GetSpotifyUrl x
+                                                    sprintf "FTFY\n[%s](%s)" url url
+                                                | None -> "Could not find it"
+
+        let message = MessageFactory.Text(messageText)
         message.TextFormat <- TextFormatTypes.Markdown
         async { return turnContext.SendActivityAsync(message, cancellationToken) } |> Async.StartAsTask :> Task
 
-    member this.GetAppleIdentifiers (appleUrl: string) =
+    member this.GetAppleIdentifiers (appleUrl: string) : Option<string> =
         let results = HtmlDocument.Load(appleUrl)
 
         let isSpan (x: HtmlNode) = x.Name() = "span"
 
-        let headerSpan = results.Descendants["h1"]
-                            |> Seq.head
-                            |> HtmlNode.descendants false isSpan
-                            |> Seq.head
-
-        headerSpan.InnerText()
+        results.Descendants["h1"]
+            |> Seq.toList
+            |> function
+                | head::_ -> head |> HtmlNode.descendants false isSpan |> Seq.head |> HtmlNode.innerText |> Some
+                | [] -> None
 
     member this.GetSpotifyUrl (name: string) =
         let googleSearch = sprintf "http://www.google.com/search?q=site:spotify.com+%s" name
@@ -41,6 +51,6 @@ type TeamsSpotifyBot =
                                     |> Option.map (fun a -> x.InnerText(), a.Value()))
             |> Seq.filter (fun (name, url) ->
                 name <> "Cached" && name <> "Similar" && url.StartsWith("/url?"))
-            |> Seq.map (fun (name, url) -> url.Substring(0, url.IndexOf("&sa=")).Replace("/url?q=", ""))
+            |> Seq.map (fun (_, url) -> url.Substring(0, url.IndexOf("&sa=")).Replace("/url?q=", ""))
             |> Seq.filter (fun url -> url.StartsWith("https://open.spotify.com"))
             |> Seq.head
