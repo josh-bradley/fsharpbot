4df4fd08d7313d24a6cc6ddd6f8078b8826a2343
Josh Bradley <josh-bradley@outlook.com>
2020-01-20 08:03:42 +1100

    Tidy up and adding user secrets

--- TeamsSpotifyBot.fs
+++ TeamsSpotifyBot.fs
@@ -15,31 +15,32 @@ type TeamsSpotifyBot =
    override this.OnMessageActivityAsync(turnContext: ITurnContext<IMessageActivity> , cancellationToken: CancellationToken) =
         turnContext.Activity.RemoveRecipientMention() |> ignore
 
-        let url = this.GetUrl (turnContext.Activity.Text.Trim())
+        let url = this.GetAppleIdentifiers (turnContext.Activity.Text.Trim()) |> this.GetSpotifyUrl
         let message = MessageFactory.Text(sprintf "FTFY\n[%s](%s)" url url)
         message.TextFormat <- TextFormatTypes.Markdown
         async { return turnContext.SendActivityAsync(message, cancellationToken) } |> Async.StartAsTask :> Task
 
-   member this.GetUrl (appleUrl: string) = 
-    let results = HtmlDocument.Load(appleUrl)
+    member this.GetAppleIdentifiers (appleUrl: string) =
+        let results = HtmlDocument.Load(appleUrl)
 
-    let isSpan (x: HtmlNode) = x.Name() = "span"
+        let isSpan (x: HtmlNode) = x.Name() = "span"
 
-    let headerSpan = results.Descendants["h1"]
-                      |> Seq.head
-                      |> HtmlNode.descendants false isSpan
-                      |> Seq.head
+        let headerSpan = results.Descendants["h1"]
+                            |> Seq.head
+                            |> HtmlNode.descendants false isSpan
+                            |> Seq.head
 
-    let name = headerSpan.InnerText()
+        headerSpan.InnerText()
 
-    let googleSearch = sprintf "http://www.google.com/search?q=site:spotify.com+%s" name
-    let results = HtmlDocument.Load(googleSearch)
+    member this.GetSpotifyUrl (name: string) =
+        let googleSearch = sprintf "http://www.google.com/search?q=site:spotify.com+%s" name
+        let results = HtmlDocument.Load(googleSearch)
 
-    results.Descendants["a"]
-        |> Seq.choose (fun x -> x.TryGetAttribute("href")
-                                |> Option.map (fun a -> x.InnerText(), a.Value()))
-        |> Seq.filter (fun (name, url) -> 
-            name <> "Cached" && name <> "Similar" && url.StartsWith("/url?"))
-        |> Seq.map (fun (name, url) -> url.Substring(0, url.IndexOf("&sa=")).Replace("/url?q=", ""))
-        |> Seq.filter (fun url -> url.StartsWith("https://open.spotify.com"))
-        |> Seq.head
+        results.Descendants["a"]
+            |> Seq.choose (fun x -> x.TryGetAttribute("href")
+                                    |> Option.map (fun a -> x.InnerText(), a.Value()))
+            |> Seq.filter (fun (name, url) ->
+                name <> "Cached" && name <> "Similar" && url.StartsWith("/url?"))
+            |> Seq.map (fun (name, url) -> url.Substring(0, url.IndexOf("&sa=")).Replace("/url?q=", ""))
+            |> Seq.filter (fun url -> url.StartsWith("https://open.spotify.com"))
+            |> Seq.head
