1b59c6e13e09dd7eab9ac9c8d67da5f08b31b942
Josh Bradley <josh-bradley@outlook.com>
2020-01-26 12:32:28 +1100

    Adding surprise gif to first call
    Checking for surprise command if we see it we will return the surprise
    gif otherwise just return the default message

---
+++ MessageBuilder.fs
@@ -0,0 +1,25 @@
+﻿module MessageBuilder
+
+open Apple
+open Spotify
+
+
+let buildMessageText (message: string) =
+    let failedMessage = "Could not find what you are looking for"
+    let r = message.Split ' '
+
+    message.Split ' '
+    |> function
+        | [|command; appleUrl|] -> 
+            let additional = if(command.Trim() = "Surprise") then "![Surprise](https://i.imgur.com/gDQk9GQ.gif)" else "FTFY"
+            appleUrl.Trim().StartsWith("https://music.apple.com")
+            |> function
+                | false -> failedMessage
+                | true -> 
+                    getAppleIdentifiers (appleUrl.Trim())
+                    |> function
+                        | None -> failedMessage
+                        | Some (title, subTitle) ->
+                            let url = getSpotifyUrl title subTitle
+                            sprintf "[%s](%s) \n\n %s" url url additional
+        | _ -> failedMessage
--- TeamsSpotifyBot.fs
+++ TeamsSpotifyBot.fs
@@ -5,8 +5,7 @@ open Microsoft.Bot.Builder
 open Microsoft.Bot.Schema
 open System.Threading
 open System.Threading.Tasks
-open Apple
-open Spotify
+open MessageBuilder
 open Microsoft.Bot.Connector
 open System
 open Microsoft.Extensions.Configuration
@@ -25,23 +24,12 @@ type TeamsSpotifyBot() =
         let conversations = new Conversations(connector)
 
         let messageText = turnContext.Activity.Text.Trim()
-                            |> this.BuildMessageText
+                            |> buildMessageText
 
         let message = MessageFactory.Text(messageText)
         message.TextFormat <- TextFormatTypes.Markdown
         let destinationConversationId = this.Configuration.["TeamsDestinationConversationId"]
+        
         async { return conversations.SendToConversationWithHttpMessagesAsync(destinationConversationId, message) } |> Async.StartAsTask :> Task
-
-    member this.BuildMessageText (appleUrl: string) =
-        let failedMessage = "Could not find what you are looking for"
-        appleUrl.StartsWith("https://music.apple.com")
-        |> function
-            | false -> failedMessage
-            | true -> getAppleIdentifiers (appleUrl)
-                        |> function
-                            | None -> failedMessage
-                            | Some (title, subTitle) ->
-                                let url = getSpotifyUrl title subTitle
-                                sprintf "FTFY: [%s](%s)" url url
 
     member val Configuration : IConfiguration = null with get, set
